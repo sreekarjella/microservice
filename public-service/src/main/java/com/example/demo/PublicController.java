d86abe5aec081c8677e6f34de33332d689fefcfa
sree1508 <142993916+sree1508@users.noreply.github.com>
2024-02-11 11:32:53 +0530

    initial commit

---
+++ public-service/src/main/java/com/example/demo/PublicController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class PublicController {
+
+	@GetMapping(path = "/public")
+	public ResponseEntity<String> demoAPI() {
+		RestTemplate restTemplate = new RestTemplate();
+		String internalAPI = "http://localhost:8092/internal";
+		return restTemplate.getForEntity(internalAPI, String.class);
+	}
+}
